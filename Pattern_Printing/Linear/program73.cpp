ddc0b0e63f383fb2d4ab741e9c33fda17df21fb8
Aniket Markad <aniket45.markad@gmail.com>
2025-12-05 11:41:58 +0530

    Add files via upload

---
+++ Pattern_Printing/Linear/program73.cpp
@@ -0,0 +1,32 @@
+// input :- 4
+// output :- $  $   $   $
+
+#include <iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    
+    for(iCnt = 1 ;iCnt <= iNo;iCnt++)
+    {
+        cout<<"$ \t";
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"enter frequency :\n";
+    cin>>iValue;
+
+    Display(iValue);
+
+    return 0;
+}
