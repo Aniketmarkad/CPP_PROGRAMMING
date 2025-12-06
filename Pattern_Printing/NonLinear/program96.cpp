251b3884320ac1c4e8c98e14e768a267c14efda2
Aniket Markad <aniket45.markad@gmail.com>
2025-12-06 13:41:44 +0530

    Add files via upload

---
+++ Pattern_Printing/NonLinear/program96.cpp
@@ -0,0 +1,41 @@
+// input :-  4. 4
+// output ;-   A    A   A   A  
+// output :-   B    B   B   B
+// output :-   C    C   C   C
+// ouput  :-   D    D   D   D
+
+#include <iostream>
+using namespace std;
+
+void Display(int iRow,int iCol)
+{
+
+    int i = 0,j = 0;
+    char ch = '\0';
+    
+    for(i = 1,ch ='a'; i<= iRow ;i++,ch++)
+    {
+        for(j= 1;j <= iCol;j++)
+        {
+            cout<<ch<<"\t";
+        }
+        cout<<"\n";
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+    
+    cout<<"enter the number of row :\n";
+    cin>>iValue1;
+
+    cout<<"enter the number of column:\n";
+    cin>>iValue2;
+    
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+ 
---
+++ Pattern_Printing/NonLinear/program98.cpp
@@ -0,0 +1,47 @@
+// input :-  4. 4
+// output ;-   *    *   *   *
+// output :-   *    o   o   *
+// output :-   *    o   o   *
+// ouput  :-   *    *   *   *
+
+#include <iostream>
+using namespace std;
+
+void Display(int iRow,int iCol)
+{
+
+    int i = 0,j = 0;
+    
+    for(i = 1; i<= iRow ;i++)
+    {
+        for(j= 1;j <= iCol;j++)
+        {
+            if(j == 1 || j == iCol || i == 1 || i == iRow)
+            {
+                cout<<"*\t";
+            }
+            else
+            {
+                cout<<"o\t";
+            }
+        }
+        cout<<"\n";
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+    
+    cout<<"enter the number of row :\n";
+    cin>>iValue1;
+
+    cout<<"enter the number of column:\n";
+    cin>>iValue2;
+    
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+ 
