18108084580b8ba37cc937eac2c87e17ffab3109
Aniket Markad <aniket45.markad@gmail.com>
2026-01-04 01:27:13 +0530

    Add files via upload

---
+++ Problem_on_Array/program118.cpp
@@ -0,0 +1,47 @@
+//Accpect input from user and allocate dynamic memory for array and return sum of odd element after use get completed deallocate the memory 
+
+#include<iostream>
+using namespace std;
+
+int SumOdd(int *Arr,int iSize)
+{
+    int iCnt= 0,iSum = 0;
+    for(iCnt = 0;iCnt < iSize ;iCnt++)
+    {
+        if(Arr[iCnt] % 2 != 0)
+        {
+            iSum = iSum + Arr[iCnt];
+        }
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iLength = 0,iCnt= 0,iRet = 0;
+    int * Arr = NULL;
+
+    cout<<"enter the size of array:\n";
+    cin>>iLength;
+
+    Arr = new int(iLength);
+
+    if(Arr == NULL)
+    {
+        cout<<"unable to allocate the memory\n";
+        return -1;
+    }
+
+    cout<<"enter the element in array:\n";
+    for(iCnt = 0;iCnt < iLength;iCnt++)
+    {
+        cin>>Arr[iCnt];
+    }
+
+    iRet = SumOdd(Arr,iLength);
+
+    cout<<"sum of odd element is :"<<iRet<<"\n";
+    delete[]Arr;
+    return 0;
+}
