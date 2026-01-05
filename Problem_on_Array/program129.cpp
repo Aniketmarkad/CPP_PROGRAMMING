4b0ad0bdd42198165e4b3989cb51368bc80cb03a
Aniket Markad <aniket45.markad@gmail.com>
2026-01-05 11:39:09 +0530

    Add files via upload

---
+++ Problem_on_Array/program129.cpp
@@ -0,0 +1,18 @@
+//accept input from user and display the division of student.
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch = '\0';
+
+    cout<<"enter your division :\n";
+
+    cin>>ch;
+
+    cout<<"your division is :"<<ch<<"\n";
+
+    return 0;
+}
