9d20736d84ce6b79b3116ba7d704119f69e2f8a3
Aniket Markad <aniket45.markad@gmail.com>
2026-01-05 11:40:17 +0530

    Add files via upload

---
+++ Problem_on_Array/program132.cpp
@@ -0,0 +1,39 @@
+//check weather the character is Capital or not.
+
+#include<iostream>
+
+using namespace std;
+
+bool CheckCapital(char ch)
+{
+    if(ch >= 'A' && ch <= 'Z')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    char ch = '\0';
+    bool bRet = false;
+
+    cout<<"Enter the character :\n";
+    cin>>ch;
+
+    bRet = CheckCapital(ch);
+
+    if(bRet == true)
+    {
+        cout<<ch<<" : is capital \n";
+    }
+    else
+    {
+        cout<<ch<<" : is not capital \n";
+    }
+
+    return 0;
+}
